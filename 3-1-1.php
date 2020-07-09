4f333ef109646520bcefb18deac27514cc3d9008
SoichiroTerasaki <67906642+SoichiroTerasaki@users.noreply.github.com>
2020-07-09 23:22:28 +0900

    Create 3-1-1.php

---
+++ 3-1-1.php
@@ -0,0 +1,18 @@
+<!--【フォーム：「名前」「コメント」の入力と「送信」ボタンが1つあるフォームを作成】-->
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>mission_3-1</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="name" placeholder="名前"> <!--コメントフォームの作成-->
+        <input type="text" name="str" placeholder="コメント">
+        <input type="submit" name="submit">
+    </form>
+    
+</body>
+</html>
